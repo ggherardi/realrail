83d2f6aa06451c6ca2847647bc61c57c77c7fcf2
Gianmattia Gherardi <giamalfred@gmail.com>
2026-08-30 11:02:04 +0200

    Refine gameplay camera framing

--- Assets/Tests/EditMode/CameraCompositionTests.cs
+++ Assets/Tests/EditMode/CameraCompositionTests.cs
@@ -20,20 +20,20 @@ namespace RealRail.Tests
             Assert.NotNull(camera);
             Assert.NotNull(player);
             Assert.NotNull(lanes);
-            Assert.AreEqual(new Vector3(0f, 13f, -11f), camera.transform.position);
-            Assert.That(Mathf.DeltaAngle(34f, camera.transform.eulerAngles.x), Is.EqualTo(0f).Within(0.01f));
-            Assert.AreEqual(42f, camera.fieldOfView);
+            Assert.AreEqual(new Vector3(0f, 8f, -8f), camera.transform.position);
+            Assert.That(Mathf.DeltaAngle(25f, camera.transform.eulerAngles.x), Is.EqualTo(0f).Within(0.01f));
+            Assert.AreEqual(45f, camera.fieldOfView);
 
             var playerViewport = camera.WorldToViewportPoint(player.transform.position);
             Assert.That(playerViewport.x, Is.EqualTo(0.5f).Within(0.01f));
-            Assert.That(playerViewport.y, Is.InRange(0.05f, 0.25f));
+            Assert.That(playerViewport.y, Is.InRange(0.14f, 0.4f));
             Assert.Greater(playerViewport.z, 0f);
 
             for (var lane = 0; lane < lanes.LaneCount; lane++)
             {
                 var spawnViewport = camera.WorldToViewportPoint(lanes.GetSpawnPosition(lane));
                 Assert.That(spawnViewport.x, Is.InRange(0f, 1f));
-                Assert.That(spawnViewport.y, Is.InRange(0.8f, 1f));
+                Assert.That(spawnViewport.y, Is.InRange(0.7f, 1f));
                 Assert.Greater(spawnViewport.z, 0f);
             }
         }
